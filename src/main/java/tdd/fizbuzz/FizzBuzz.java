39e284791bcb64ca4b85df3939445aa4757cb89f
abylak <ABylak@pl.imshealth.com>
2017-06-20 10:55:48 +0200

    Part II - green

--- src/main/java/tdd/fizbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizbuzz/FizzBuzz.java
@@ -1,8 +1,22 @@
 package tdd.fizbuzz;
 
 public class FizzBuzz {
-
     public String fizzBuzz(int number) {
-        return "";
+
+        StringBuilder sb = new StringBuilder();
+
+        if (number == 0) {
+            return sb.toString();
+        }
+
+        if (number%3 == 0) {
+            sb.append("Fizz");
+        }
+
+        if (number%5 == 0) {
+            sb.append("Buzz");
+        }
+
+        return sb.toString();
     }
 }
